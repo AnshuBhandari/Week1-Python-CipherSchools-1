818ee8fc0ed84ba6123da15ff589e910126b1558
anugyaverma99 <119095704+anugyaverma99@users.noreply.github.com>
2022-12-02 01:20:13 +0530

    Update class5-Operators and Expressions-CipherSchools.py

--- class5-Operators and Expressions-CipherSchools.py
+++ class5-Operators and Expressions-CipherSchools.py
@@ -7,9 +7,9 @@ print(10//3) #Floor Division
 print(10%3)
 print(2*3)
 print(2**3) #Exponentiation
-print("Manoranjan"+"Sahoo")
+print("Anugya"+"Verma")
 print("abc"*6)
-print("Manoranjan" "" "Sahoo") #String Concatenation
+print("Anugya" "" "Verma") #String Concatenation
 
 #Comparision Operators
 print(1==2)
@@ -30,10 +30,10 @@ print(type(True))
 print(bool(1))
 #Short Curcuiting
 '''
-a or b=a(if a is truthy)
-a or b=b(if a is falsy)
-a and b=b(if a is truthy)
-a and b=a(if a is falsy)
+a or b=a(if a is truth)
+a or b=b(if a is false)
+a and b=b(if a is truth)
+a and b=a(if a is false)
 '''
 '''
 True or b=True
@@ -43,7 +43,7 @@ False or b=False
 '''
 #Without knowing b we get the result
 #Mathematical Beauty!
-print("Mano" and 6)
+print("Anu" and 6)
 print(""and 6)
 print(1.6 or "")
 print("" or 2.5)
